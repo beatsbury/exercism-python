c8a527d714d1eba52616061a8c1eed4423270b43
beatsbury <beatsbury@hotmail.com>
2019-11-30 14:56:37 +0500

    completed rna_transcription xrc

--- rna-transcription/rna_transcription.py
+++ rna-transcription/rna_transcription.py
@@ -1,2 +1,8 @@
 def to_rna(dna_strand):
-    pass
+    # GCTA -> CGAU
+    dna = 'GCTA'
+    rna = 'CGAU'
+    rna_value = ''
+    for nucleotide in dna_strand:
+        rna_value += rna[dna.index(nucleotide)]
+    return rna_value
